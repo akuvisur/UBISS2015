ff598ca190e1419c8fadfacce47478b6d5920241
Aku <aku.visuri@invian.fi>
2015-06-11 13:14:45 +0300

    Worked on the notificationemitter..

--- app/src/main/java/com/example/skoolknot/awesomea/ContextAnalyzer.java
+++ app/src/main/java/com/example/skoolknot/awesomea/ContextAnalyzer.java
@@ -4,5 +4,14 @@ package com.example.skoolknot.awesomea;
  * Created by skoolknot on 6/10/15.
  */
 public class ContextAnalyzer {
+    public static final int IGNORE = 0;
+    public static final int TOAST = 1;
+    public static final int NOTIFICATION = 2;
+    public static final int POPUP = 3;
+    public static final int LOCK_SCREEN_MSG = 4;
 
+    public static int analyze(String appName) {
+        // returns the notification type for the app 'AppName'
+        return ContextAnalyzer.POPUP;
+    }
 }
--- app/src/main/java/com/example/skoolknot/awesomea/NotificationEmitter.java
+++ app/src/main/java/com/example/skoolknot/awesomea/NotificationEmitter.java
@@ -1,9 +1,17 @@
 package com.example.skoolknot.awesomea;
 
+import android.app.Activity;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.PixelFormat;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
+import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +22,7 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
+import android.widget.TextView;
 import android.widget.Toast;
 
 /**
@@ -24,48 +33,118 @@ public class NotificationEmitter {
     private static WindowManager windowManager;
     private static View popupView;
 
+    public static void createNotification(Context c, String appName, View v, Drawable appImage) {
+        int type = ContextAnalyzer.analyze(appName);
+        switch(type) {
+            case ContextAnalyzer.IGNORE:
+                Log.d("CREATOR", "Notifiation ignored");
+            case ContextAnalyzer.TOAST:
+                Log.d("CREATOR", "Creating a toast");
+                emitToast(c, appName, appImage, v);
+            case ContextAnalyzer.NOTIFICATION:
+                Log.d("CREATOR", "Creating a notification");
+                emitNotification(c, appName, appImage, v);
+            case ContextAnalyzer.POPUP:
+                emitPopup(c, appName, appImage, v);
+                Log.d("CREATOR", "Creating a popup");
+            case ContextAnalyzer.LOCK_SCREEN_MSG:
+                Log.d("CREATOR", "Creating a lockscreenmsg");
+                emitLockMsg(c, appName, appImage, v);
+        }
+    }
+
+    private static void emitToast(Context c, String appName, Drawable appImage, View v) {
+        // emit a custom notification
+    }
+
+    private static void emitLockMsg(Context c, String appName, Drawable appImage, View v) {
+        AlertDialog alertDialog = new AlertDialog.Builder(c).create();
+        alertDialog.getWindow().setType(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
+        alertDialog.show();
+    }
+
+    private static void emitPopup(Context c, String appName, Drawable appImage, View v) {
+        // emit a popup
+    }
+
+    private static void emitNotification(Context c, String appName, Drawable appImage, View v) {
+        // emit a notification
+    }
+
     public static void emitDummyNotification(Context c) {
+
+        Intent okIntent = new Intent(c, null);
+        okIntent.setAction("notifications_pressed");
+        okIntent.putExtra("notifications_accepted", "dummy_app");
+
+        PendingIntent pOkIntent = PendingIntent.getService(c, 0, okIntent, 0);
+
+        Intent igIntent = new Intent(c, null);
+        igIntent.setAction("notifications_pressed");
+        igIntent.putExtra("notifications_ignored", "dummy_app");
+
+        PendingIntent pIgIntent = PendingIntent.getService(c, 0, igIntent, 0);
+
         mBuilder =
                 new NotificationCompat.Builder(c)
                         .setSmallIcon(R.drawable.alert)
                         .setContentTitle("New notification")
-                        .setContentText("Hi there!");
-        // Sets an ID for the notification
-        int mNotificationId = 001;
+                        .setContentText("Hi there!")
+                        .addAction(R.drawable.ok, "Ok", pOkIntent)
+                        .addAction(R.drawable.ignore, "Ignore", pIgIntent);
+
         // Gets an instance of the NotificationManager service
         NotificationManager mNotifyMgr =
                 (NotificationManager) c.getSystemService(Context.NOTIFICATION_SERVICE);
         // Builds the notification and issues it.
-        mNotifyMgr.notify(mNotificationId, mBuilder.build());
+        mNotifyMgr.notify(0, mBuilder.build());
     }
 
     public static void emitDummyToast(Context c) {
-        Toast.makeText(c, "Hi there!", Toast.LENGTH_LONG).show();
+        LayoutInflater inflater = (LayoutInflater) c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layout = inflater.inflate(R.layout.notification, null);
+
+        TextView appName = (TextView) layout.findViewById(R.id.appName);
+        ImageButton okb = (ImageButton) layout.findViewById(R.id.okButton);
+        ImageButton igb = (ImageButton) layout.findViewById(R.id.ignoreButton);
+
+        appName.setText("Dummy application");
+        okb.setImageResource(R.drawable.ok);
+        igb.setImageResource(R.drawable.ignore);
+
+        Toast toast = new Toast(c.getApplicationContext());
+        toast.setGravity(Gravity.CENTER_HORIZONTAL, 0, 0);
+        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 360);
+        toast.setGravity(Gravity.BOTTOM, 0, 20);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(layout);
+        toast.show();
     }
 
     public static void emitDummmyPopup(Context c) {
         windowManager = (WindowManager) c.getSystemService(Context.WINDOW_SERVICE);
 
-        ImageButton okButton = new ImageButton(c);
-        okButton.setImageResource(R.drawable.ok);
+        LayoutInflater inflater = (LayoutInflater) c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layout = inflater.inflate(R.layout.notification, null);
 
-        ImageButton ignoreButton = new ImageButton(c);
-        ignoreButton.setImageResource(R.drawable.ignore);
+        TextView appName = (TextView) layout.findViewById(R.id.appName);
+        ImageButton okb = (ImageButton) layout.findViewById(R.id.okButton);
+        ImageButton igb = (ImageButton) layout.findViewById(R.id.ignoreButton);
 
-        LinearLayout ll = new LinearLayout(c);
-        ll.addView(okButton);
-        ll.addView(ignoreButton);
+        appName.setText("Dummy application");
+        okb.setImageResource(R.drawable.ok);
+        igb.setImageResource(R.drawable.ignore);
 
         final WindowManager.LayoutParams params = new WindowManager.LayoutParams(
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.TYPE_PHONE,
                 WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
-                PixelFormat.TRANSLUCENT);
+                PixelFormat.OPAQUE);
 
         params.gravity = Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL;
 
-        windowManager.addView(ll, params);
-
+        windowManager.addView(layout, params);
     }
+
 }
